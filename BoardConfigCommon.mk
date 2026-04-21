5f181e92e1821627ff3bc2839c04cf364a6223f3
ppanzenboeck <panz.development@gmail.com>
2026-04-21 21:47:23 +0200

    add ViPER4Android

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -265,3 +265,5 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/sm8650-common/BoardConfigVendor.mk
+
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
