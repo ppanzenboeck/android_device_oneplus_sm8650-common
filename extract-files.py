b386185438ad57e40cf599e2d4ef9eec8ccc4a0a
Kill3rEz <98993017+Kill3rEz@users.noreply.github.com>
2026-06-17 16:19:33 +0200

    sm8650-common: Fix HDR encoder and decoder in media_codecs_pineapple.xml
    Added regex replacements to fix HDR encoder and decoder configurations in media_codecs_pineapple.xml.
    
    Signed-off-by: DqrKnzx <DavidYonatan9@hotmail.com>

--- extract-files.py
+++ extract-files.py
@@ -152,6 +152,39 @@ blob_fixups: blob_fixups_user_type = {
         .add_line_if_missing('libQnnSystem.so')
         .add_line_if_missing('libQnnHtpV75Stub.so')
         .add_line_if_missing('libQnnGpu.so'),
+    'vendor/etc/media_codecs_pineapple.xml': blob_fixup()
+        #FIX HDR ENCODER
+        .regex_replace(
+            r'<!--\s*<MediaCodec name="c2\.qti\.dv\.encoder" type="video/dolby-vision">',
+            r'<MediaCodec name="c2.qti.dv.encoder" type="video/dolby-vision">'
+        )
+        .regex_replace(
+            r'(<Limit name="performance-point-7680x4320" value="30" />\s*)</MediaCodec>\s*-->',
+            r'\1    <Feature name="profile-and-level" value="256-8" />\n'
+            r'            <Feature name="profile-and-level" value="256-256" />\n'
+            r'            <Feature name="profile-and-level" value="256-1024" />\n'
+            r'        </MediaCodec>'
+        )
+        #FIX HDR DECODER
+        .regex_replace(
+            r'<!--\s*<MediaCodec name="c2\.qti\.dv\.decoder" type="video/dolby-vision" >',
+            r'<MediaCodec name="c2.qti.dv.decoder" type="video/dolby-vision" >'
+        )
+        .regex_replace(
+            r'(<Limit name="performance-point-8192x4320" value="48" />\s*)</MediaCodec>',
+            r'\1    <Feature name="profile-and-level" value="256-8" />\n'
+            r'            <Feature name="profile-and-level" value="256-256" />\n'
+            r'            <Feature name="profile-and-level" value="256-1024" />\n'
+            r'        </MediaCodec>'
+        )
+        #FIX HDR DECODER SECURE
+        .regex_replace(
+            r'(<Limit name="performance-point-4096x2304" value="120" />\s*)</MediaCodec>\s*-->',
+            r'\1    <Feature name="profile-and-level" value="256-8" />\n'
+            r'            <Feature name="profile-and-level" value="256-256" />\n'
+            r'            <Feature name="profile-and-level" value="256-1024" />\n'
+            r'        </MediaCodec>'
+        ),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
